51b43c24d959dd256d8ebd049d4c631e9d177072
MIniHahn <30485886+MIniHahn@users.noreply.github.com>
2017-08-14 09:21:03 +0800

    Add files via upload

---
+++ mini.py
@@ -0,0 +1,15 @@
+print    ("请在20内猜测一数，您有1-次机会")
+import random
+a=random.eandint(0,20)
+
+i=range( 9 ):
+b = input ((“请输入您猜测的数"))
+
+if b > a
+    print ("大了")
+	print ("您还有i-1次机会")
+elif b < a
+    print ("小了")
+    print ("您还有i-1次机会")
+else:
+    print ("恭喜您，猜对了！") 	
